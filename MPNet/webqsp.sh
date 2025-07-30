c13e557f89c5b77f090de1c091036c50d5297985
Hyemin Yang <92848713+Hyemeee@users.noreply.github.com>
2025-07-30 20:57:06 +0900

    Update webqsp.sh

--- MPNet/webqsp.sh
+++ MPNet/webqsp.sh
@@ -9,17 +9,16 @@ DIR="$( cd "$( dirname "$0" )" && cd .. && pwd )"
 echo "working directory: ${DIR}"
 
 if [ -z "$DATA_DIR" ]; then
-  DATA_DIR="/home/hyemin/model/my_model/data/${TASK}"
+  DATA_DIR="/data/${TASK}"
 fi
 
-#"microsoft/mpnet-base"
 
 python3 -u train.py \
 --pretrained-model "microsoft/mpnet-base" \
 --pooling mean \
---train_path "/home/hyemin/model/my_model/data/webqsp/train_goldenpath_newtotal_0416.jsonl" \
---train_graph_path "/home/hyemin/shared_data/webqsp/train_max80000_subgraph.pkl" \
---triple2id_1 "/home/hyemin/shared_data/webqsp/webqsp_triple2id.pkl" \
+--train_path "/data/webqsp/train_goldenpath.jsonl" \
+--train_graph_path "/data/webqsp/total_graph_webqsp.jsonl" \
+--triple2id_1 "/data/webqsp_triple2id.pkl" \
 --batch_size 1 \
 --print-freq 100 \
 --max_num_neg 50 \
@@ -27,4 +26,4 @@ python3 -u train.py \
 --epochs 6 \
 --workers 2 \
 --max-to-keep 3 "$@" \
---output-dir "/home/hyemin/model/my_model/BACKPACK/ENT_PRUNER/webqsp/subgraph"
+--output-dir "/ckpt/webqsp"
