57b4170392991140f408715983fdce355ebc5d25
Hyemin Yang <92848713+Hyemeee@users.noreply.github.com>
2025-07-30 20:43:49 +0900

    Update cwq.sh

--- MPNet/cwq.sh
+++ MPNet/cwq.sh
@@ -9,17 +9,17 @@ DIR="$( cd "$( dirname "$0" )" && cd .. && pwd )"
 echo "working directory: ${DIR}"
 
 if [ -z "$DATA_DIR" ]; then
-  DATA_DIR="/home/hyemin/model/my_model/data/${TASK}"
+  DATA_DIR="=/data/${TASK}"
 fi
 
-#"microsoft/mpnet-base"
+
 
 python3 -u train.py \
---pretrained-model "Alibaba-NLP/gte-large-en-v1.5" \
+--pretrained-model "microsoft/mpnet-base"\
 --pooling mean \
---train_path "/home/hyemin/model/my_model/data/cwq/train_concat_goldenpath_newtotal_0416.jsonl" \
---train_graph_path "/home/hyemin/shared_data/cwq/total_graph_cwq.jsonl" \
---triple2id_1 "/home/hyemin/shared_data/cwq/cwq_triple2id.pickle" \
+--train_path "/data/cwq/train_goldenpath.jsonl" \
+--train_graph_path "/data/cwq/total_graph_cwq.jsonl" \
+--triple2id_1 "/data/cwq/cwq_triple2id.pickle" \
 --batch_size 1 \
 --print-freq 1000 \
 --max_num_neg 50 \
@@ -27,4 +27,4 @@ python3 -u train.py \
 --epochs 5 \
 --workers 2 \
 --max-to-keep 3 "$@" \
---output-dir "/home/hyemin/model/my_model/BACKPACK/ENT_PRUNER/cwq/alibaba_totalgraph"
+--output-dir "/ckpt/cwq"
